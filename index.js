b89157ce2d25589dc7da0dde127a90268aa7e836
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-07-03 18:12:00 -0400

    Finished

---
+++ index.js
@@ -0,0 +1,53 @@
+const url = "https://developer.nps.gov/api/v1/parks?"
+const api = "sR5HKTDhcCuMpFI0TOCuakykfMJvzWEV10oL0agb";
+
+function createRenderString(obj) {
+    let parkArray = [];
+
+    parkArray = obj.data.map(park => {
+        return `<li><h1>${park.name}</h1><p>${park.description}</p><a href="${park.url}">Link</a></li>`
+    });
+    return parkArray;
+}
+
+function renderResults(str) {
+    $('#results-list').html(str);
+}
+
+function createRequestString(arr) {
+    let arrString = '';
+    let frmt = arr.split(',');
+    arrString = frmt.map((state) => {
+        return `stateCode=${state}&`
+    });
+
+    arrString = arrString.join('');
+    return arrString;
+}
+
+function init() {
+    // $("#states").load("states.html");
+    $('form').on('submit', function (e) {
+
+        e.preventDefault();
+        $('#results-list').html('');
+        $('.loading').toggleClass('hidden');
+        fetch(`${url}${createRequestString($('#states').val())}limit=${$('#max').val()}&api_key=${api}`, {
+            headers: {
+                'accept': 'application/json',
+            }
+
+        })
+            .then(response => response.json())
+            .then(data => createRenderString(data))
+            .then(str => {
+                $('.loading').toggleClass('hidden');
+                renderResults(str);
+            })
+            .catch((error) => {
+                alert('Error getting parks.');
+            });
+    })
+}
+
+$(init)
